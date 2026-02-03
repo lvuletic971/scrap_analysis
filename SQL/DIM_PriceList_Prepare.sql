2248396b4703db6e14b97a7f744f569b2ee55fb6
lvuletic971 <lvuletic971@gmail.com>
2026-02-03 20:36:44 +0100

    Update DIM_PriceList_Prepare.sql

--- SQL/DIM_PriceList_Prepare.sql
+++ SQL/DIM_PriceList_Prepare.sql
@@ -8,4 +8,4 @@ SELECT
 	CnCena AS CenaC
 FROM Cenik c --to je view na KG
 WHERE c.CnRazTipCn ='4' AND c.CnDatSpremembe BETWEEN '2021-10-01' AND '2021-12-31' 
-ORDER BY DatumC DESC, ArtiklC, ArtiklCS
+ORDER BY DatumC DESC, ArtiklC
